b016a9d5bb7a128d3b57643af40df3a21b75bc72
aditiee <65465599+aditiee@users.noreply.github.com>
2020-12-05 15:26:22 +0530

    Add files via upload

---
+++ Day1.sln/Basic/BasicProgram1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Round(6.5));
+            Console.WriteLine(Math.Round(11.5));
+        }
+    }
+}
